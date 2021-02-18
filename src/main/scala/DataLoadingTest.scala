3fd6eefebbe756753aecf94d5877e5d7fa3f255f
Tong Panrong <panrong.tong@staff.main.ntu.edu.sg>
2021-02-18 14:57:17 +0800

    add jts type

--- src/main/scala/DataLoadingTest.scala
+++ src/main/scala/DataLoadingTest.scala
@@ -3,6 +3,8 @@ import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.serializer.KryoSerializer
 import org.datasyslab.geospark.serde.GeoSparkKryoRegistrator
 import org.apache.spark.sql.SparkSession
+import com.vividsolutions.jts.geom.{Coordinate, GeometryFactory}
+
 
 
 object DataLoadingTest {
@@ -29,6 +31,7 @@ object DataLoadingTest {
 
     loadCsv()
     loadCsvAndParse()
+    loadCsvAndParseWithJts()
 
     sc.stop()
 
@@ -46,7 +49,6 @@ object DataLoadingTest {
         .csv(csvFile)
 
       val trajRDD = df.rdd
-        .repartition(numPartitions)
         .filter(row => row(8).toString.split(',').length >= 4) // each trajectory should have no less than 2 recorded points
 
       val resRDD = trajRDD.map(row => {
@@ -64,11 +66,40 @@ object DataLoadingTest {
       })
 
       resRDD.take(1)
+
       df.unpersist()
       trajRDD.unpersist()
       resRDD.unpersist()
     }
 
+    def loadCsvAndParseWithJts(): Unit = {
+      val df = spark.read.option("header", "true")
+        .option("numPartitions", numPartitions)
+        .csv(csvFile)
 
+      val trajRDD = df.rdd
+        .filter(row => row(8).toString.split(',').length >= 4) // each trajectory should have no less than 2 recorded points
+
+      val resRDD = trajRDD.map(row => {
+        val tripID = row(0).toString
+        val taxiID = row(4).toString.toLong
+        val startTime = row(5).toString.toLong
+        val pointsString = row(8).toString
+        var pointsCoord = new Array[Double](0)
+        for (i <- pointsString.split(',')) pointsCoord = pointsCoord :+ i.replaceAll("[\\[\\]]", "").toDouble
+        var points = new Array[Coordinate](0)
+        for (i <- 0 to pointsCoord.length - 2 by 2) {
+          points = points :+ new Coordinate(pointsCoord(i), pointsCoord(i + 1))
+        }
+        val geometryFactory = new GeometryFactory()
+        geometryFactory.createLineString(points)
+      })
+
+      resRDD.take(1)
+
+      df.unpersist()
+      trajRDD.unpersist()
+      resRDD.unpersist()
+    }
   }
 }
